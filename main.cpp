2e2b968c0b0c899c6ff7f8c8763c90afdba96219
Joe <joeacarstairs@gmail.com>
2021-04-04 09:11:56 +0100

    Added an "Are you sure?" step to the quit command.

--- main.cpp
+++ main.cpp
@@ -43,7 +43,22 @@ void CmdHandler::setHandlerDefault()
 CmdHandler::Returns defaultCmdHandler(std::wstring userInput)
 {
 	if (userInput == L"quit" || userInput == L"exit") {
-		return CmdHandler::Returns::QUIT_APP;
+
+		std::wcout << "Are you sure you want to quit? [Y/N]\n";
+		std::wstring userInput = L"";
+		std::wcin >> userInput;
+
+		if (userInput == L"Y" || userInput == L"y"
+			|| userInput == L"YES" || userInput == L"YEs" || userInput == L"YeS" || userInput == L"Yes" || userInput == L"yES" || userInput == L"yEs" || userInput == L"yeS" || userInput == L"yes"
+			|| userInput == L"0" || userInput == L"true")
+		{
+			std::wcout << L"Exiting app...\n";
+			return CmdHandler::Returns::QUIT_APP;
+		}
+
+		std::wcout << L"Not exiting app.\n";
+		return CmdHandler::Returns::SUCCESS;;
 	}
+
 	return CmdHandler::Returns::CMD_NOT_RECOGNISED;
 }
