9d852b6b5d560016503d476374a6254cb3fa8a9b
Joe <joeacarstairs@gmail.com>
2021-04-04 09:01:06 +0100

    Created CmdHandler namespace, with setHandler and setHandlerDefault functions, and made enum class Returns for acceptable return values for handlers.

---
+++ CmdHandler.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+namespace CmdHandler {
+
+	enum class Returns {
+		SUCCESS,
+		CMD_NOT_RECOGNISED,
+		QUIT_APP
+	};
+
+	void setHandler(Returns(*handler)(std::wstring));
+	void setHandlerDefault();
+}
