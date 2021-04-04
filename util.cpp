e36171ded360c13748f28e191d021cb8d37391c0
Joe <joeacarstairs@gmail.com>
2021-04-04 14:47:37 +0100

    Adds toLower() to accompany toUpper().

--- util.cpp
+++ util.cpp
@@ -23,8 +23,30 @@ std::wstring toUpper(std::wstring wstr)
 	return result;
 }
 
+std::wstring toLower(std::wstring wstr)
+{
+	std::wstring result = L"";
+	for (unsigned int i = 0; i < wstr.length(); i++) {
+		result.push_back(std::tolower(wstr[i]));
+	}
+	return result;
+}
+
 bool isYes(std::wstring str)
 {
 	str = toUpper(str);
 	return (str == L"Y" || str == L"YES" || str == L"1" || str == L"TRUE");
+}
+
+template<typename T> inline bool shareAnyElems(std::vector<T> v1, std::vector<T> v2)
+{
+	for (int i = 0; i < v1.size(); i++)
+	{
+		for (int j = 0; j < v2.size(); j++)
+		{
+			if (v1[i] == v2[j])
+				return true;
+		}
+	}
+	return false;
 }
--- util.h
+++ util.h
@@ -5,4 +5,6 @@
 std::wstring getUserInputLine(std::wistream &stream = std::wcin);
 bool getUserYesNo();
 std::wstring toUpper(std::wstring wstr);
+std::wstring toLower(std::wstring wstr);
 bool isYes(std::wstring wstr);
+template<typename T> bool shareAnyElems(std::vector<T> v1, std::vector<T> v2);
