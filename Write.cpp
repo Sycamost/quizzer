12fea16282570d198b887f6dac5f0d385bb38465
Joe <joeacarstairs@gmail.com>
2021-04-08 13:12:06 +0100

    Declares cancelCurrentWrite() and fixes typo.

--- Write.cpp
+++ Write.cpp
@@ -3,7 +3,7 @@
 #include "util.h"
 #include "WriteFlashcard.h"
 
-const DECLARE_CMD_FUNC(startWriting) {
+DECLARE_CMD_FUNC(startWriting) {
 
 	if (args.size() == 0)
 		return CmdHandler::Returns::TOO_FEW_ARGS;
@@ -18,4 +18,15 @@ const DECLARE_CMD_FUNC(startWriting) {
 	// ... other types
 
 	return CmdHandler::Returns::INVALID_ARGS;
+};
+
+DECLARE_CMD_FUNC(cancelCurrentWrite) {
+	std::wcout << L"\nAre you sure you want to cancel adding the current card? [Y/N]\n";
+	if (getUserYesNo())
+	{
+		WriteFlashcardStage::setValue(WriteFlashcardStage::Stage::NEW_CARD);
+		return CmdHandler::Returns::SUCCESS;
+	}
+	WriteFlashcardStage::setValue(WriteFlashcardStage::Stage::FRONT);
+	return CmdHandler::Returns::SUCCESS;
 };
