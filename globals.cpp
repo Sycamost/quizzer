f3df4650956fc6b55e6bd2ae37fca6d1aa82ee18
Joe <joeacarstairs@gmail.com>
2021-04-11 14:55:06 +0100

    Adds global start-of-tags string

--- globals.cpp
+++ globals.cpp
@@ -1,6 +1,7 @@
 #include "globals.h"
 
 const std::wstring Globals::fileEscapeChar = L"%%";
+const std::wstring Globals::fileStartOfTags = Globals::fileEscapeChar + L"tags";
 const std::wstring Globals::optionCaseSensitive = L"case_sensitive";
 const std::wstring Globals::horizontalRule = L"--------------------------------";
 const std::wstring Globals::horizontalDoubleRule = L"================================";
