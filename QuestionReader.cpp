e4a326be71eeaf4f31b46cfa66533bc9148200ba
Joe <joeacarstairs@gmail.com>
2021-04-11 15:01:14 +0100

    Implements QuestionReader::read().

---
+++ QuestionReader.cpp
@@ -0,0 +1,37 @@
+#include "QuestionReader.h"
+#include "Option.h"
+#include "util.h"
+#include "globals.h"
+
+Question* QuestionReader::read(std::wifstream& stream)
+{
+	std::vector<Option> options = Option::readOptions(stream);
+
+	while (!stream.eof())
+	{
+		std::wstring line = getInputLine(stream);
+
+		if (line == L"")
+		{
+			return _constructCurrent();
+		}
+
+		else if (line == Globals::fileStartOfTags)
+		{
+			setStage(Stage::TAGS);
+			continue;
+		}
+
+		if (_stage == Stage::CHILD_DATA)
+		{
+			_readChildData(line);
+			continue;
+		}
+
+		else if (_stage == Stage::TAGS)
+		{
+			_tags.push_back(line);
+			continue;
+		}
+	}
+}
