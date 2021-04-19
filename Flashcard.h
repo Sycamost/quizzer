26d994e3fe52784f19b71ed8be6524ef401081bf
Joe <joeacarstairs@gmail.com>
2021-04-19 18:35:37 +0100

    Removes redundant writer function in Flashcard (now taken over by FlashcardWriter).

--- Flashcard.h
+++ Flashcard.h
@@ -11,7 +11,6 @@ private:
 	std::wstring _answer;
 	bool _caseSensitive;
 	static const std::wstring _optCaseSensitive;
-	void writeChildData(std::wofstream& stream);
 public:
 	Flashcard(std::wstring question, std::wstring answer, bool caseSensitive, std::vector<std::wstring> tags = std::vector<std::wstring>());
 	std::wstring getQuestion();
